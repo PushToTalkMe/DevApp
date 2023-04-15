330b7648f6d0f892b80e5e75e29a74c74f5b0795
PushToTalk <x_killer90@mail.ru>
2023-04-15 18:27:38 +0300

    Added pages of courses

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,6 +1,5 @@
 import styles from "./layout.module.css";
 import { LayoutProps } from "./layout.props";
-import cn from "classnames";
 import { Header } from "./header/header";
 import { Sidebar } from "./sidebar/sidebar";
 import { Footer } from "./footer/footer";
@@ -8,14 +7,12 @@ import { FunctionComponent } from "react";
 
 export const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import { Button, Htag, P, Rating, Tag } from "@/components";
 import { withLayout } from "@/layout/layout";
+import { GetStaticProps } from "next";
 import { useState } from "react";
+import axios from "axios";
+import { MenuItem } from "@/interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(4);
   return (
     <>
@@ -23,8 +26,32 @@ function Home(): JSX.Element {
       </Tag>
       <Tag color="primary">Мал</Tag>
       <Rating rating={rating} isEditable setRating={setRating}></Rating>
+      {menu.map((m) => (
+        <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+      ))}
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + "/api/top-page/find",
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
